9ffe4ae8594efd69987a8415d57dce018560de08
Bashir Ahmad <107115756+BashirAhmadZ@users.noreply.github.com>
2022-06-09 09:04:16 +0530

    First Time Commit

---
+++ SanShopWebParent/SanShopBackend/src/main/java/com/sanshop/admin/SanShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanShopBackendApplication.class, args);
+	}
+
+}
---
+++ SanShopWebParent/SanShopFrontEnd/src/main/java/com/sanshop/site/SanShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.sanshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanShopFrontEndApplication.class, args);
+	}
+
+}
